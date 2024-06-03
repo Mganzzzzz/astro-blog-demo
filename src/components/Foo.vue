adf574e50fa295d90ee15f3d0d51a32a5f15474f
mganz <yue.hong@dtminds.com>
2024-06-03 15:40:28 +0800

    feat: fundamentals

---
+++ src/components/Foo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="onClick">foo</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const onClick = () => {
+  console.log("debug onClick");
+};
+</script>
+
+<style scoped></style>
