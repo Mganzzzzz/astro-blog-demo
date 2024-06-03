54c6ff1dc395bcd9129bf755fc3a9c1169a51deb
mganz <yue.hong@dtminds.com>
2024-06-03 18:20:49 +0800

    chore: mjs - js

--- astro.config.js
+++ astro.config.js
@@ -1,18 +1,30 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: node({
-        mode: 'standalone',
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  integrations: [
+    vue({
+      jsx: true,
+      devtools: { launchEditor: "webstorm" },
     }),
-    integrations: [vue({
-        jsx: true,
-        devtools: {launchEditor: "webstorm"},
-    }),
-        tailwind()],
+    tailwind(),
+  ],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "entry.[hash].js",
+          chunkFileNames: "chunks/chunk.[hash].js",
+          assetFileNames: "assets/asset.[hash][extname]",
+        },
+      },
+    },
+  },
 });
