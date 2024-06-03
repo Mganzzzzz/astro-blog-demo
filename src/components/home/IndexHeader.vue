fdf469a64a2a7e5d3c6bd34d30241e69786f3c34
mganz <yue.hong@dtminds.com>
2024-06-03 17:03:58 +0800

    chore: ant

---
+++ src/components/home/IndexHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <Carousel :after-change="onChange">
+    <div><h3>1</h3></div>
+    <div><h3>2</h3></div>
+    <div><h3>3</h3></div>
+    <div><h3>4</h3></div>
+  </Carousel>
+  <Space>
+    <Button type="primary" danger @click="onBtnClick">Primary</Button>
+    <Button type="primary" danger @click="onBtnClick">Primary</Button>
+  </Space>
+</template>
+<script lang="ts" setup>
+import { Button, Carousel, Space } from "ant-design-vue";
+const onChange = (current: number) => {
+  console.log(current);
+};
+
+const onBtnClick = () => {
+  console.log("debug onBtnClick");
+};
+</script>
+<style scoped lang="less">
+/* For demo */
+:deep(.slick-slide) {
+  text-align: center;
+  height: 160px;
+  line-height: 160px;
+  background: #364d79;
+  overflow: hidden;
+}
+
+:deep(.slick-slide h3) {
+  color: #fff;
+}
+</style>
