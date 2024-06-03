6feca6fb30099bbc03cf6522b42939e85541b23b
mganz <yue.hong@dtminds.com>
2024-06-03 15:51:39 +0800

    chore: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
-
+import node from '@astrojs/node';
 
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: node({
+        mode: 'standalone',
+    }),
     integrations: [vue({
         jsx: true,
         devtools: {launchEditor: "webstorm"},
